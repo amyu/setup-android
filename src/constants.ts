e9a190949b2a5d13847b6d9b8002b6dfa2c679c7
Burakhan Öztaş <burakhanoztas@araskargo.com.tr>
2024-04-19 11:15:42 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -16,7 +16,7 @@ export const COMMANDLINE_TOOLS_LINUX_URL = `https://dl.google.com/android/reposi
 export const COMMANDLINE_TOOLS_MAC_URL = `https://dl.google.com/android/repository/commandlinetools-mac-10406996_latest.zip`
 export const COMMANDLINE_TOOLS_WINDOWS_URL = `https://dl.google.com/android/repository/commandlinetools-win-10406996_latest.zip`
 
-export const HOME = os.homedir()
+export const HOME = process.env.ANDROID_HOME || os.homedir()
 // github hosted runnerのubuntu-latestではすでにandroid directoryが存在しているため.をつけて回避
 export const ANDROID_HOME_DIR = path.join(HOME, '.android')
 
