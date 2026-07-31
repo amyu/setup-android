9e1e0b0af407676406361ce5ad3ec0bca173544c
amyu <amyu051213@gmail.com>
2026-08-01 01:20:31 +0900

    Migrate linting and formatting to Oxc

--- src/setup-android.ts
+++ src/setup-android.ts
@@ -1,61 +1,55 @@
-import * as core from '@actions/core'
-import {addPath} from './add-path.js'
-import {restoreCache} from './cache.js'
-import type {Versions} from './constants.js'
-import * as constants from './constants.js'
-import {installAndroidSdk} from './installer.js'
-import {normalizeSdkVersions} from './sdk-version.js'
+import * as core from "@actions/core";
+
+import { addPath } from "./add-path.js";
+import { restoreCache } from "./cache.js";
+import type { Versions } from "./constants.js";
+import * as constants from "./constants.js";
+import { installAndroidSdk } from "./installer.js";
+import { normalizeSdkVersions } from "./sdk-version.js";
 
 async function run(): Promise<void> {
   try {
-    const sdkVersion = normalizeSdkVersions(
-      core.getMultilineInput(constants.INPUT_SDK_VERSION),
-      {warn: message => core.warning(message)}
-    )
-    const buildToolsVersion = core.getMultilineInput(
-      constants.INPUT_BUILD_TOOLS_VERSION
-    )
-    const ndkVersion = core.getInput(constants.INPUT_NDK_VERSION)
-    const cmakeVersion = core.getInput(constants.INPUT_CMAKE_VERSION)
-    const commandLineToolsVersion = core.getInput(
-      constants.INPUT_COMMAND_LINE_TOOLS_VERSION
-    )
-    const cacheDisabled = core.getBooleanInput(constants.INPUT_CACHE_DISABLED)
-    const cacheKey = core.getInput(constants.INPUT_CACHE_KEY)
+    const sdkVersion = normalizeSdkVersions(core.getMultilineInput(constants.INPUT_SDK_VERSION), {
+      warn: (message) => core.warning(message),
+    });
+    const buildToolsVersion = core.getMultilineInput(constants.INPUT_BUILD_TOOLS_VERSION);
+    const ndkVersion = core.getInput(constants.INPUT_NDK_VERSION);
+    const cmakeVersion = core.getInput(constants.INPUT_CMAKE_VERSION);
+    const commandLineToolsVersion = core.getInput(constants.INPUT_COMMAND_LINE_TOOLS_VERSION);
+    const cacheDisabled = core.getBooleanInput(constants.INPUT_CACHE_DISABLED);
+    const cacheKey = core.getInput(constants.INPUT_CACHE_KEY);
 
     const versions: Versions = {
       sdkVersion,
       buildToolsVersion,
       ndkVersion,
       cmakeVersion,
-      commandLineToolsVersion
-    }
+      commandLineToolsVersion,
+    };
 
-    core.startGroup('Environment details for Android SDK')
-    addPath(versions)
-    core.endGroup()
+    core.startGroup("Environment details for Android SDK");
+    addPath(versions);
+    core.endGroup();
 
     if (!cacheDisabled) {
-      core.startGroup('Restored Android SDK from Cache')
-      const restoreCacheEntry = await restoreCache(versions, cacheKey)
-      core.endGroup()
+      core.startGroup("Restored Android SDK from Cache");
+      const restoreCacheEntry = await restoreCache(versions, cacheKey);
+      core.endGroup();
       if (restoreCacheEntry) {
-        return Promise.resolve()
+        return Promise.resolve();
       }
     }
 
-    core.startGroup('Installed Android SDK')
-    await installAndroidSdk(versions)
-    core.endGroup()
+    core.startGroup("Installed Android SDK");
+    await installAndroidSdk(versions);
+    core.endGroup();
   } catch (error) {
+    core.info("To see the logs executed by sdkmanager, set ACTIONS_STEP_DEBUG to true");
     core.info(
-      'To see the logs executed by sdkmanager, set ACTIONS_STEP_DEBUG to true'
-    )
-    core.info(
-      'https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/enabling-debug-logging'
-    )
-    if (error instanceof Error) core.setFailed(error.message)
+      "https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/enabling-debug-logging",
+    );
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+void run();
